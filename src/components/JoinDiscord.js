1d4e89182d185b0a262521223f1b13e9c4d29792
drbarzyk <drbarzyk@vt.edu>
2021-09-09 06:18:50 -0400

    Updated UI - Animated gradient

--- src/components/JoinDiscord.js
+++ src/components/JoinDiscord.js
@@ -4,7 +4,7 @@ import DiscordIcon from './DiscordIcon'
 function JoinDiscord() {
     return (
         <a href="https://discord.gg/X8BENaR9Ft" className="join-discord btn">
-            <span>Join Our Network</span>
+            <span>Join The Network</span>
             <DiscordIcon color="white" height="32px"/>
         </a>
     )
